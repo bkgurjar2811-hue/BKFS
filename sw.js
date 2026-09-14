03db7aa7aabea238f469435e5c6f08aac9718b22
bkgurjar2811-hue <bkgurjar.2811@gmail.com>
2026-09-14 19:08:23 +0530

    Add BKFS browser prototype with explicit demo limitations

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='bkfs-v1';const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
